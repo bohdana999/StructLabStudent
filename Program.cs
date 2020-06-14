c63c093cc99cfe253a5fd656031751077dce0f2a
Ryba Bohdana <danushkaagneshka@gmail.com>
2020-06-15 01:47:27 +0300

    Finished LAB 5

--- Program.cs
+++ Program.cs
@@ -10,14 +10,24 @@ namespace struct_lab_student
     {
         static Student[] ReadData(string fileName)
         {
-            // TODO   implement this method.
-            // It should read the file whose fileName has been passed and fill 
+            string[] lines = File.Exists(fileName) ? File.ReadAllLines(fileName) : new string[0];
+            var students = new Student[lines.Length];
+            for (var i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student(lines[i]);
+            }
+            return students;
         }
 
         static void runMenu(Student[] studs)
         {
-            // TODO   implement this method
-            // It should call method(s) for concrete variant(s)
+            for (var i = 0; i < studs.Length; i++)
+            {
+                if (studs[i].InformaticsMark == 5)
+                {
+                    Console.WriteLine(studs[i].surName + " " + studs[i].AverageMark);
+                }
+            }
         }
 
         static void Main(string[] args)
--- Student.cs
+++ Student.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Text;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace struct_lab_student
 {
     struct Student
     {
+        const char SEPARATOR = ' ';
+
         public string surName;
         public string firstName;
         public string patronymic;
@@ -18,8 +19,31 @@ namespace struct_lab_student
 
         public Student(string lineWithAllData)
         {
-            // TODO   you SHOULD IMPLEMENT constructor with exactly this signature
-            // lineWithAllData is string contating all data about one student, as described in statement
+            var regex = new Regex(@"\s+");
+            lineWithAllData = regex.Replace(lineWithAllData, SEPARATOR.ToString());
+            var splited = lineWithAllData.Split(SEPARATOR);
+
+            surName = splited[0];
+            firstName = splited[1];
+            patronymic = splited[2];
+            sex = splited[3][0];
+            dateOfBirth = splited[4];
+            mathematicsMark = splited[5][0];
+            physicsMark = splited[6][0];
+            informaticsMark = splited[7][0];
+            scholarship = Convert.ToInt32(splited[8]);
+        }
+
+        public int MathematicsMark => ParseMark(mathematicsMark);
+        public int PhysicsMark => ParseMark(physicsMark);
+        public int InformaticsMark => ParseMark(informaticsMark);
+        public double AverageMark => (MathematicsMark + PhysicsMark + InformaticsMark) / 3.0;
+        
+
+        private static int ParseMark(char mark)
+        {
+            bool result = int.TryParse(mark.ToString(), out var intMark);
+            return result ? intMark : 2;
         }
     }
 }
